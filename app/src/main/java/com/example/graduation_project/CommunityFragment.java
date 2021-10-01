18ae068198f74557aa2a39e0f58baf4f181380de
yelim0308 <71305230+yelim0308@users.noreply.github.com>
2021-10-01 16:55:51 +0900

    info add

--- app/src/main/java/com/example/graduation_project/CommunityFragment.java
+++ app/src/main/java/com/example/graduation_project/CommunityFragment.java
@@ -1,48 +1,27 @@
 package com.example.graduation_project;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class CommunityFragment extends Fragment implements View.OnClickListener {
+public class CommunityFragment extends Fragment {
     ViewGroup viewGroup;
 
-    public CommunityFragment() {
-
-    }
-
-
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_community, container, false);
-
-        Button btn = (Button) view.findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), CommunityActivity.class);
-                startActivity(intent);
-
-            }
-        });
-        return view;
-
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_community, container, false);
+        return viewGroup;
     }
 
-    @Override
-    public void onClick(View view) {
-
-    }
 }
-
 
 
 
--- app/src/main/java/com/example/graduation_project/InfoFragment.java
+++ app/src/main/java/com/example/graduation_project/InfoFragment.java
@@ -4,16 +4,27 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -21,6 +32,9 @@ import java.io.InputStream;
 public class InfoFragment extends Fragment{
     ViewGroup viewGroup;
     ImageView imageProfile;
+    TextView userName;
+
+    private static final String TAG = "MainActivity"; //logout
 
     @Nullable
     @Override
@@ -39,14 +53,31 @@ public class InfoFragment extends Fragment{
             }
         });
 
+
+        viewGroup.findViewById(R.id.logout).setOnClickListener(onClickListener);
+        viewGroup.findViewById(R.id.info_edit).setOnClickListener(onClickListener);
+
+        //set user name
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String name = user.getEmail();
+            userName = viewGroup.findViewById(R.id.userId);
+            userName.setText(name);
+
+        }
+        //set user name
+
+
+
         return viewGroup;
     }
 
+    //image
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 101) {
-            if(resultCode == getActivity().RESULT_OK) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == getActivity().RESULT_OK) {
+            if(resultCode == 101) {
                 ContentResolver contentResolver = getActivity().getContentResolver();
                 try {
                     InputStream inputStream = contentResolver.openInputStream(data.getData());
@@ -59,5 +90,32 @@ public class InfoFragment extends Fragment{
             }
         }
     }
+    //image
+
+
+    //logout
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.logout:
+                    FirebaseAuth.getInstance().signOut();
+                    myStartActivity(SignUpActivity.class);
+                    break;
+
+                case R.id.info_edit:
+                    myStartActivity(MemberInitActivity.class);
+                    break;
+            }
+        }
+    };
+
+    private void myStartActivity(Class c) {
+        Intent intent = new Intent(getActivity(), c);
+
+        startActivity(intent);
+    }
+
+    //logout
     
 }
