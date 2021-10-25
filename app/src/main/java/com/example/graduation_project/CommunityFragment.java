4e08903953ca4535e9f9b80bb0d2bb02568f1976
KKS <73399251+kuchic0918@users.noreply.github.com>
2021-10-25 10:33:39 +0900

    10/25 kks

--- app/src/main/java/com/example/graduation_project/CommunityFragment.java
+++ app/src/main/java/com/example/graduation_project/CommunityFragment.java
@@ -11,7 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class CommunityFragment extends Fragment implements View.OnClickListener {
+public class CommunityFragment extends Fragment {
     ViewGroup viewGroup;
 
     public CommunityFragment() {
@@ -37,14 +37,7 @@ public class CommunityFragment extends Fragment implements View.OnClickListener 
 
     }
 
-    @Override
-    public void onClick(View view) {
-
-    }
 }
-
-
-
 
 
 
--- app/src/main/java/com/example/graduation_project/GalleryActivity.java
+++ app/src/main/java/com/example/graduation_project/GalleryActivity.java
@@ -1,21 +1,26 @@
 package com.example.graduation_project;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class GalleryActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
+
 
 
     @Override
@@ -23,14 +28,48 @@ public class GalleryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
 
+        if (ContextCompat.checkSelfPermission(GalleryActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(GalleryActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    1);
+            if (ActivityCompat.shouldShowRequestPermissionRationale(GalleryActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            } else {
+                startToast("권한을 허용해 주세요");
+            }
+        } else {
+            recyclerInit();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    recyclerInit();
+                } else {
+                    finish();
+                    startToast("권한을 허용해 주세요");
+                }
+            }
+        }
+    }
+
+    private void recyclerInit(){
+
         final int numberOfColumns = 3;
 
-        recyclerView = findViewById(R.id.recyclerView);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
 
 
-       mAdapter = new GalleryAdapter(this, getImagesPath(this));
+        RecyclerView.Adapter mAdapter = new GalleryAdapter(this, getImagesPath(this));
         recyclerView.setAdapter(mAdapter);
     }
     public ArrayList<String> getImagesPath(Activity activity) {
@@ -61,6 +100,9 @@ public class GalleryActivity extends AppCompatActivity {
             listOfAllImages.add(PathOfImage);
         }
         return listOfAllImages;
+    }
+    private void startToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
     // ...
